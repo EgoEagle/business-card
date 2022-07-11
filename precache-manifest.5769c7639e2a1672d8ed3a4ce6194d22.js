dc5092c0ebcde41520a149f79463d69637c29da9
Tony <TonyLin3591@gmail.com>
2022-07-11 14:25:32 -0400

    Updates

---
+++ precache-manifest.5769c7639e2a1672d8ed3a4ce6194d22.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4515c2aa191f25353f24",
+    "url": "/business-card/static/css/main.5c6831cc.chunk.css"
+  },
+  {
+    "revision": "4515c2aa191f25353f24",
+    "url": "/business-card/static/js/main.4515c2aa.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/business-card/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "c3490619314eb5f938e6",
+    "url": "/business-card/static/js/2.c3490619.chunk.js"
+  },
+  {
+    "revision": "5f4f840c45df01d80faf",
+    "url": "/business-card/static/js/runtime~main.5f4f840c.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/business-card/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "9cd8c58eb9a34545ce9cda90a16e1460",
+    "url": "/business-card/static/media/pic.9cd8c58e.jpg"
+  },
+  {
+    "revision": "ef1f3fd2719a2336f1c85664b189b11b",
+    "url": "/business-card/index.html"
+  }
+];
