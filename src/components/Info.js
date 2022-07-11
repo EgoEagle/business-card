b680d50d13bd564072d7d90089c3aa2d2d430a07
Tony <TonyLin3591@gmail.com>
2022-07-11 15:00:15 -0400

    deploy

--- src/components/Info.js
+++ src/components/Info.js
@@ -12,9 +12,13 @@ export default function Info(){
                <h1 className="name"> Tony Lin</h1>
                <h2 className="title"> Software Engineer</h2>
                <div className ="webdiv">
-                    <a href="https://egoeagle.github.io/intro/"> <button className="gitlink">My Page</button></a>
+                    <a href="https://egoeagle.github.io/intro/"> 
+                         <button className="gitlink">My Page</button>
+                    </a>
                     
-                    <a href="https://www.linkedin.com/in/tony-lin-b5742420a/"> <button className="linkedin">LinkedIn</button></a>
+                    <a href="https://www.linkedin.com/in/tony-lin-b5742420a/"> 
+                         <button className="linkedin">LinkedIn</button>
+                    </a>
                     
                </div>
                <div className="text">
